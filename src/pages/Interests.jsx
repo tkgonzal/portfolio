c7b49497f422324665ffd9dda8b1eb57a3d34896
Traven Gonzales <travengonzales@gmail.com>
2023-08-05 02:27:01 -0700

    feat(interests): Fleshed out Interests page

--- src/pages/Interests.jsx
+++ src/pages/Interests.jsx
@@ -2,7 +2,60 @@ function Interests() {
     return (
         <>
             <div className="title">Interests</div>
-            This is where the interests go
+            <h1>A Few of my Most Favourite Things</h1>
+            <p className="interests--blurb">
+                I'm a worldly guy with some worldly tastes. Take a quick
+                peep at some things I like.
+            </p>
+
+
+            <h2 className="interests--header">Music</h2>
+            <p className="interests--blurb">
+                Music is probably the thing I'm most invested in. I try to
+                listen to a lot of music but my favourite genres are Hip-Hop 
+                and Midwest Emo. Some of my favourite artists include JPEGMAFIA,
+                mom jeans., 100 Gecs, and D'Angelo.
+            </p>
+
+            <h2 className="interests--header">Art</h2>
+            <p className="interests--blurb">
+                In my free time I'm a hobby artist, though nowadays I tend to 
+                appreciate art more often than make it. Here's a very minimal
+                gallery of pieces I've made.
+            </p>
+
+            <h2 className="interests--header">Custom Keyboards</h2>
+            <p className="interests--blurb">
+                During the pandemic, I got into the hobby of custom mechanical
+                keyboards because I didn't have enough expensive hobbies to 
+                drain my wallet. I've built quite a few boards since getting 
+                into the hobby but my current daily driver is a:
+            </p>
+            <h3 className="interests--subheader">QK60</h3>
+            <ul>
+                <li>QwertyKeys 60 in HHKB</li>
+                <li>CanonKeys Neapolitan Switches</li>
+                <ul>
+                    <li>Lubed with Tribosys 3203</li>
+                    <li>Filmed with Deskeys Film</li>
+                </ul>
+                <li>AEBoards Staebies V2.1 Stabilizers</li>
+                <ul>
+                    <li>Lubed with Krytox 205g0</li>
+                    <li>Wires lubed with Dielectric Grease</li>
+                </ul>
+                <li>Cherry Spellbook Keycaps</li>
+            </ul>
+            
+            <h2 className="interests--header">Games</h2>
+            <p className="interests--blurb">
+                Games are arguably the reason I became interested in
+                Computer Science in the first place. Some of my favourite
+                video games include Persona 3 Portable, Mother 3, and
+                Undertale. Haven't had the time to play video games 
+                much as of late, but this year I've recently gotten into
+                and have been obssessed with the TTRPG Pathfinder 2e.
+            </p>
         </>
     )
 }
