8df9f99ff9ba07bffeb6eca273af2b8f62c7816e
Traven Gonzales <travengonzales@gmail.com>
2023-08-07 13:38:43 -0700

    feat(album-data): Added an album entry to albums.js and jpg support to the project

--- src/data/Interests/albums.js
+++ src/data/Interests/albums.js
@@ -1,5 +1,12 @@
-const albums = [
+import whitePonyImg from "../../assets/albums/white-pony.jpg"
 
+const albums = [
+    {
+        imgSrc: whitePonyImg,
+        title: "White Pony",
+        artist: "Deftones",
+        href: "https://www.youtube.com/playlist?list=PLC80P4gsPr-bLGUNpAdxytEsA5y8PUsbL"
+    }
 ]
 
 export default albums
