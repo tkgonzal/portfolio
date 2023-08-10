cec102093e6aa7f7733a7d6b98988d20bbcc9808
Traven Gonzales <travengonzales@gmail.com>
2023-08-09 18:25:47 -0700

    chore(style): Fixed minor styling inconsistencies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css"
 
 function Header() {
     return (
-        <header id="#">
+        <header>
             <h1 className="header--title">tkgonzal</h1>
         </header>
     )
