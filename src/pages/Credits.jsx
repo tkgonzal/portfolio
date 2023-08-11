87bd570c7b8433b853f2b23c892c8b0bde608824
Traven Gonzales <travengonzales@gmail.com>
2023-08-11 16:44:55 -0700

    feat(credits): Added content to Credits page

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -4,7 +4,33 @@
 function Credits() {
     return (
         <>
-            This is where the credits go.
+            <h1>Thank You for the Heart</h1>
+            <p>
+                This portfolio was all coded by me, and a majority
+                of the assets were also made by yours truly, but for
+                the few things I didn't take, I'd like to give proper
+                thanks
+            </p>
+
+            <h2>Interests</h2>
+            <p>
+                The design for the display of my current music
+                rotation is based on <a href="https://topsters.org/" target="_blank">Topsters</a>,
+                a popular music sharing format online. All album images are taken either
+                from <a href="https://www.discogs.com/" target="_blank">Discogs</a>, if the album
+                links to a YouTube playlist, or <a href="https://bandcamp.com/" target="_blank">Bandcamp</a>, if
+                the album links to a Bandcamp page.
+            </p>
+
+            <h2>Footer</h2>
+            <ul>
+                <li>
+                    The stamp of Persona 5's Futaba's All Out Attack animation
+                    is pulled from tumblr user <a 
+                        href="https://www.tumblr.com/dizzy-lights/724850595343876096/i-promised-so-i-shall-deliver-the-other-phantom"
+                    >dizzy-light</a>'s blog
+                </li>
+            </ul>
         </>
     )
 }
